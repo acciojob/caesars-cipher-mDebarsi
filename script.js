1c2b6f6b880ac52cc520f3e0c6d8e49502cc9a6f
DEBARSI MONDAL <141905446+mDebarsi@users.noreply.github.com>
2023-11-06 16:17:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -32,15 +32,15 @@ const lookup = {
 };
 
 function rot13(encodedStr) {
-  let decodedArr = []; // Your Result goes here
-  // Only change code below this line
-
-  return; //return decodedArr
-}
-
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
-
-// console.log(rot13("SERR YBIR? NPPVBWBO"));
-
-// Do not change this line
+ str = str.toUpperCase();
+	let output="";
+	for(let i=0;i<encodedStr.length; i++)
+		{
+			if(lookup[str[i]]){
+				output += lookup[encodedStr[i]];
+			} else{
+				output +=encodedStr[i];
+			}
+		}
+	return output;
 window.rot13 = rot13;
